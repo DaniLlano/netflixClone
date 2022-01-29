4ec2d9942181464a54a758b5642043bf1cb18d58
danillano <danillano97@gmail.com>
2022-01-29 00:20:50 -0300

    update models Movie

--- api/models/Movie.js
+++ api/models/Movie.js
@@ -9,7 +9,7 @@ const MovieSchema = new mongoose.Schema({
         trailer: {type: String},
         video: {type: String},
         year: {type: String},
-        limit: {type: String},
+        limit: {type: Number},
         genre: {type: String},
         isSeries: {type: Boolean, default: false},
     },
