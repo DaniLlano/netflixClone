7a6b8fe9ddcb0f2e14cbc25e5148a9424c99023c
Dani <danillano97@gmail.com>
2022-01-31 23:54:39 -0300

    login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 // register
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
     });
 
     try {
@@ -17,5 +18,14 @@ router.post("/register", async (req, res) => {
     }
     
 });
+
+// login
+router.post("login", (req, res) => {
+    try {
+
+    } catch(err) {
+        res.status(500).json(err)
+    }
+} )
 
 module.exports = router;
