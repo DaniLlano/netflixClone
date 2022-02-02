4fd4de367ccae2d075471b0d5adaa47545d06796
danillano <danillano97@gmail.com>
2022-02-02 00:37:55 -0300

    verifyToken

---
+++ api/routes/users.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJs = require("crypto-js");
+
+// update
+
+router.put("/:id", async (req, res) => {
+
+})
+
+// delete
+
+// get
+
+// get all
+
+// get user stats
